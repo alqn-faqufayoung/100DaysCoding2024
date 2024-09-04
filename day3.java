d8e578a3e09a026cd9f0e688c741b8b72bc0d539
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-04 22:01:30 +0800

    day3.java

---
+++ day3.java
@@ -0,0 +1,9 @@
+
+public class Day3 {
+    public static void main(String[] args) {
+        System.out.println("Nama=Alan alvito fahrurozy");
+        System.out.println("Nim=D0224525");
+        System.out.println("Kelas=A");
+        System.out.println("Alamat=Dusun Tulunagung,Desa Sumberjo,Kec.Wonomulyo");
+    }
+}
