1d7c796ad3af7cf2a21682a4c6e05693b9b46f4e
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-05 22:47:06 +0800

    Coding100day

---
+++ day4.java
@@ -0,0 +1,11 @@
+public class Coding100Day {
+
+    public static void main(String[] args) {
+        System.out.println("Nama = Alan alvito F. \n");
+        System.out.println("Kelas = A24 \n");
+        System.out.println("NIM = D0224525 \n");
+        
+        System.out.printf("Nama = %s%nUmur = %d Tahun%nTinggi badan = %dcm%nBerat badan = %dkg%n","Alan alvito F.", 22, 175, 65);
+    }
+    
+}
