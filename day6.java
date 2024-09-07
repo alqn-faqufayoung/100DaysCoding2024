9e1f51d3f43f4416946d0cede9c9fde964eb581e
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-07 22:21:39 +0800

    Coding100Day

---
+++ day6.java
@@ -0,0 +1,15 @@
+public class java {
+    public static void main(String[] args) {
+        //deklarasi
+        
+        string Nama = "Alan alvito fahrurozy";//code ini tipe data untuk teks
+        int Umur = 22;                       //code ini tipe data untuk bilangan bulat
+        float Berat badan = 65,5f;          //code ini tipe data untuk bilangan desimal yg lebih presisi dan harus di akhiri f setelah angka
+        double Tinggi badan = 175;         //code ini hampir sama dengan float, namun lebih detail dan presisi yg tinggi
+
+
+    //makan astor, sambil bergaya
+    //halo kak mentor, jangan bosen yaa..;)
+    }
+    
+}
