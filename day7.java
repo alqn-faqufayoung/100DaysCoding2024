568c3eb8c20a70a79f5bcb39ca2c58c2f46da387
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-08 22:05:46 +0800

    Variabel : Inisialisasi

---
+++ day7.java
@@ -0,0 +1,12 @@
+public class Day7 {
+    //Deklarasi: Mengenalkan variabel ke program tanpa memberikan nilai
+   //Inisialisasi: Memberikan nilai awal pada variabel saat atau setelah deklarasi
+    
+    public static void main(String[] args) {
+        String Nama = "Alan alvito fahrurozy";//Variabel nama di inisialisasikan dengan string Alan alvito fahrurozy
+        int Umur = 22;                       //Variabel umur di inisialisasikan dengan nilai integral 22
+        float Berat badan = 65,5f;          //Variabel berat badan di inisialisasikan dengan angka 65,5 
+        double Tinggi badan = 175;         //Variabel Tinggi badan di inisialisasikan dengan angka 175
+    }
+    
+}
