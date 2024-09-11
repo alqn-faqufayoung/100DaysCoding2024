5bc15b5524411935c8918bac6ccf1c74e93c7d5d
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-11 23:12:03 +0800

    Tipe Data : byte

---
+++ day10.java
@@ -0,0 +1,13 @@
+package day10;
+
+public class Coding100Days {
+    public static void main(String[] args) {
+        //Tipe Data Byte
+        byte TDByte = 123;
+        /*tipe data ini bisa kita gunakan ketika kita ingin menggunakan angka kecil
+        dan menghemat ruang memori,
+        karna 1 byte (8bite) dengan rentan -128 hingga 127.
+        */
+        System.out.println("byte : " + TDByte);
+    }
+}
