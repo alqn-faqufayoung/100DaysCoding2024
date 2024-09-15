29f567a850061b4510572aea39d4a8832524f65a
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-15 23:11:17 +0800

    Type data : long

---
+++ day14.java
@@ -0,0 +1,18 @@
+package coding100days;
+
+public class day14 {
+
+    public static void main(String[] args) {
+    /*
+         > ukuran 8 byte (64bite).
+         > Type data long tepatnya digunakan ketika kita bekerja
+         dengan angka bulat besar/sangat kecil, yang tidak bisa ditangani oleh int.
+         > rentang nilai/angka sebagai berikut:
+    */
+        long min = -9223372036854775808L;
+        long max = 9223372036854775807L;
+        
+        System.out.println("Range mulai dari :"+ min);
+        System.out.println("Sampai           :"+ max);
+       }
+      }
