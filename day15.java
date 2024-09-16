d9bb60efa8b979dc6740d2ddc3759342eed3c67d
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-16 23:16:50 +0800

    tipe data : double

---
+++ day15.java
@@ -0,0 +1,18 @@
+package Coding100Day;
+
+public class Day_15 {
+
+    public static void main(String[] args) {
+        //Tipe data double :
+        //ukuran 8byte(64bite)
+        /*tipe data yang hanya bisa di isi oleh bilangan desimal atau bilangan berkoma,
+        fungsinya hampir sama dengan float, tapi double bisa lebih kecil/besar lagi..
+*/
+        // contoh :
+
+        double a = -9.9;
+        double b = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999.9;
+        System.out.println("rasa cintaku kepadamu = " + b);
+        System.out.println("sikapmu kepadaku = " + a);
+    }
+}
