9739fa863fdcef9b70b718913c24cf305809c8a1
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-17 20:00:43 +0800

    tipe data : float

---
+++ day16.java
@@ -0,0 +1,20 @@
+package coding100days;
+
+public class day16 {
+  public static void main(String[] args) {
+    //Type data float:
+    /*ukuran 4byte (32bite)
+    tipe data yg teruntuk bilangan desimal/berkoma.
+    hampir sama dengan tipe data double,
+    kalo float ukurannya lebih kecil, dan di akhir desimal harus di kasih tanda f,
+    supaya program tau kalo menggunakan tipe data float.
+    Contoh sebagai berikut
+    */
+    
+    float kecil =-99999999999999999999999999999999999999.9f;
+    float besar = 9.99999999999999999999999999999999999999f;
+    
+    System.out.println("Angka paling kecil = "+ kecil);
+    System.out.println("Angka paling kecil = "+ besar);
+ }
+}
