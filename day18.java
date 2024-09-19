4007257aa03697f9a5c72665dde9c0813f502896
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-19 23:42:26 +0800

    Tipe Data : boolean

---
+++ day18.java
@@ -0,0 +1,18 @@
+package coding100days;
+ 
+public class day18 {
+ 
+    public static void main(String[] args) {
+        //Tipe data bolean >
+        /*tipe data ini hanya bisa menyimpan 2 nilai,
+        yaotu true(benar) & false(salah) */
+        
+        boolean cinta = true;
+        boolean mang_eak = false;
+        
+        System.out.println("apakah Alan mencintai dia..? " + cinta);
+        System.out.println("apakah dia mencintai Alan..? " + mang_eak);
+        
+    }
+    
+}
