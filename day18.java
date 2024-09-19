b8ba4e13bbddf85cb1a4b61cfdd00a9ea861f9e4
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-19 22:41:41 +0800

    Tipe Data : char

---
+++ day18.java
@@ -0,0 +1,12 @@
+package coding100days;
+
+public class day18 {
+
+    public static void main(String[] args) {
+           
+    char kelas = 'A';
+    
+    System.out.println("Saya Alan masuk di Informatika kelas: " + kelas);
+    
+    } 
+}
