d66dd74021da6b24416712249198c7094bf19180
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-20 14:42:11 +0800

    Tipe data refrence : String

---
+++ day19.java
@@ -0,0 +1,14 @@
+package coding100days;
+ 
+public class day19 {
+ 
+    public static void main(String[] args) {
+ //Tipe data refrence > String >
+ /* Menyimpan kumpulan karakter sperti huruf, teks, angka, simbol,dll
+  & pada saat meng.inisialisasi.kan harus di kasih tanda petik 2 ("xxx")
+ */
+ String nama = "BOTAX dan HITAM";
+        System.out.println("ciri-ciri Alan : ");
+ System.out.println("mas-mas Jawir " + nama + " pake topi mancing");
+ }  
+}
