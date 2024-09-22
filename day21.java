0d81bdbd44fab6604dfd6c703f1d7368c122d31a
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-22 23:54:45 +0800

    Tipe Data Reference : Short

---
+++ day21.java
@@ -0,0 +1,13 @@
+package coding100days;
+ 
+public class day21 {
+ 
+    public static void main(String[] args) {
+        //Tipe Data Reference : Short
+        // tipe data referensi atau objek di Java yang membungkus nilai primitif
+        
+        short primitif = 12345;
+        Short non_primitif = primitif;
+        System.out.println("angka kesukaan = " + primitif);
+ }
+}
