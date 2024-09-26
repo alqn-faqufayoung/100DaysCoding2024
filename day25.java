87a1b44c90c341254f4c979677d1b418c325bccc
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-26 19:44:29 +0800

    Tipe Data Reference : Double

---
+++ day25.java
@@ -0,0 +1,20 @@
+package coding100days;
+ 
+public class day25 { 
+    
+    public static void main(String[] args) {
+//Tipe Data Reference : Double 
+/*memiliki ukuran 64-bit dan bisa menyimpan angka desimal
+dalam rentang sekitar 4.9E-324 hingga 1.7976931348623157E308.
+Gunakan Double ketika kamu butuh angka desimal dengan presisi tinggi dalam bentuk objek,
+misalnya saat bekerja dengan koleksi objek
+atau ketika kamu ingin menggunakan metode/metode yang disediakan oleh class Double.
+*/
+Double bungkus = -123456789.0;
+Double jajan = 0.987654321;
+
+        System.out.println("bungkus rokok = " + bungkus);
+        System.out.println("rokok perbungkus = " + jajan);
+
+ }
+}
