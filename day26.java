263fe8787ca5bc20abedab5e430183511ebc1708
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-27 23:14:24 +0800

    Tipe Data Reference : Character

---
+++ day26.java
@@ -0,0 +1,17 @@
+package coding100days;
+ 
+public class day26 {
+    public static void main(String[]args){
+       //Tipe Data Reference : Character 
+       /*Character di Java adalah versi objek dari tipe data primitif char.
+       Tipe ini digunakan untuk menyimpan karakter tunggal dalam bentuk objek,
+       yang berguna saat kamu membutuhkan karakter untuk digunakan dalam struktur data
+       */
+       Character alan = 'A';
+       Character alvito = 'L';
+       Character fahrurozy = 'N';
+       
+        System.out.println("inisial pelaku = "+alan+alvito+alan+fahrurozy);
+        System.out.println("Kelas = informatika "+alan+"24");
+ }
+}
