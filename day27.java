31e77ee13494a3c337c9f9e30e23c1b76887b572
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-29 19:38:47 +0800

    Evaluasi Besar

---
+++ day27.java
@@ -0,0 +1,17 @@
+package coding100days;
+
+public class day27 {
+//Evaluasi Besar
+
+    public static void main(String[] args) {
+ 
+    System.out.println("Selamat Datang");
+    
+     final int jumlahFilm = 50;
+     String judulFilm = "Ironman";
+     Short tahunRilis = 2010;
+     judulFilm = "From";
+     
+     System.out.printf("Film berjudul %s di rilis pada tahun %d dan terdapat %d film pada katalog saat ini %n",judulFilm,tahunRilis,jumlahFilm);
+ }
+}
