afef55290e0411fff949a622392f03491862ae1b
alqn-faqufayoung <alanalvito21@gmail.com>
2024-09-29 20:19:04 +0800

    Tipe Data Reference : Boolean

---
+++ day28.java
@@ -0,0 +1,19 @@
+package coding100days;
+
+public class day28 {
+
+    public static void main(String[] args) {
+//Tipe Data Reference : Boolean
+/*tipe data yang hanya dapat menyimpan dua nilai, yaitu:
+- true (benar) atau
+- false (salah).
+Tipe data ini digunakan untuk mewakili kondisi yang hanya memiliki dua kemungkinan.
+*/
+Boolean ganteng = true;
+Boolean cinta = false;
+
+System.out.println("Apakah Alan gagah dan pemberani..!? "+ ganteng + " (Alan memanglah gagah dan pemberani)");
+System.out.println("Apakah Alan pernah di cintai..!? "+ cinta + " (Waduhhh, Sayangnya Alan belum pernah diCintai)");
+    System.out.printf("NB: Awogwog%n");
+ } 
+}
