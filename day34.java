0f55085cbf0ef3774e8b7fb00da96dcd0dcac597
alqn-faqufayoung <alanalvito21@gmail.com>
2024-10-06 00:23:01 +0800

    Input dari keyboard

--- day34.java
+++ day34.java
@@ -2,22 +2,23 @@ package coding100days;
  import java.util.Scanner;
   public class day34{
    public static void main(String[] args){
-       //Input dari keyboard :
-       //Dengan Scanner, kamu bisa dengan mudah mengambil berbagai jenis input dari pengguna sesuai kebutuhan program.
+     //Input dari keyboard :
+      //Dengan Scanner, kamu bisa dengan mudah mengambil berbagai jenis input dari pengguna sesuai kebutuhan program.
 
-    Scanner in1 = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
+    
      System.out.print("Nama lengkap = ");
-      String Nama = in1.nextLine();
+      String Nama = in.nextLine();
         
-         Scanner in2 = new Scanner(System.in);
-          System.out.print("Umur Sekarang = ");
-           int Umur = in2.nextInt();
-        
-             Scanner in3 = new Scanner(System.in);
-              System.out.print("Alamat lengkap = ");
-               String Alamat = in3.nextLine();
-        
-             System.out.printf("PERKENALKAN :%nNama Saya = %s Saya Berumur = %d Tahun & Alamat Saya =  %s%n",Nama,Umur,Alamat);
+         System.out.print("Umur Sekarang = ");
+          int Umur = in.nextInt();
+          
+             in.nextLine(); // membersihkan buffer sebelum mengambil input String berikutnya
+              System.out.printf("Alamat lengkap = ");
+               String Alamat = in.nextLine();
+               
+                 System.out.println(" ");
+                  System.out.printf("PERKENALKAN :%nNama Saya = %s Saya Berumur = %d Tahun & Alamat Saya =  %s%n",Nama,Umur,Alamat);
 
  }
 }
