b4c64998790dc90701e804c4a427a482e48ac2ef
alqn-faqufayoung <alanalvito21@gmail.com>
2024-10-07 20:23:22 +0800

    Operator Aritmatika : Perkalian, Pembagian serta Modulo

---
+++ day36.java
@@ -0,0 +1,18 @@
+package coding100days;
+ 
+public class day36 {
+ 
+    public static void main(String[] args) {
+     // Operator Aritmatika : Perkalian, Pembagian serta Modulo
+     int cowok = 555;
+      int cewek = 123;
+      
+     int siswa = cowok*cewek;
+      int siswi = cowok/cewek;
+       int dosen = cowok%cewek;
+     
+     System.out.println("Perkalian = "+siswa);
+      System.out.println("Pembagian = "+siswi);
+       System.out.println("sisa bagi/modulo = "+dosen);
+  }
+ }
