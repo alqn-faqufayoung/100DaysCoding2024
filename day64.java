dcb3a6a2da8cccca525b7c4f6c7051819a8450eb
alqn-faqufayoung <alanalvito21@gmail.com>
2024-11-05 07:54:11 +0800

    break label

--- day64.java
+++ day64.java
@@ -1,18 +1,19 @@
 package coding100days;
 
 public class day64 {
+
     // break label
     /* digunakan untuk menghentikan loop bersarang (nested loop) yang berada di dalam lebih dari satu loop.
        Dengan menggunakan label, kita dapat menghentikan beberapa loop sekaligus dan keluar dari loop yang ditandai dengan label tersebut.
        Tanpa label, break hanya akan menghentikan loop yang paling dalam (loop terdekat). */
-    
+
     public static void main(String[] args) {
         for (int i = 1; i <= 99; i++) {
             for (int j = 1; j <= 99; j++) {
                 if (j == 6) {
                     break;
                 }
-                System.out.print(i);
+                System.out.println("i= " + i + " j= " + j);
             }
             if (i == 5) {
                 break;
@@ -21,12 +22,12 @@ public class day64 {
         System.out.println(" ");
         System.out.println("====================================================");
         x:
-        for (int x = 1; x <= 10; x++) {
+        for (int x = 1; x <= 99; x++) {
             for (int y = 1; y <= 5; y++) {
-                if (x == 6 ) {
+                if (x <= 5 && y <= 5) {
                     break x;
                 }
-                System.out.print(x);
+                System.out.println("x= " + x + " y= " + y);
             }
         }
         System.out.println(" ");
