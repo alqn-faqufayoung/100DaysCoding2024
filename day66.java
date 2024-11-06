190627010bbbf6ad87b5888e82c4dd203f952c14
alqn-faqufayoung <alanalvito21@gmail.com>
2024-11-07 01:03:42 +0800

    Pola Persegi dan Persegi Panjang\

--- day66.java
+++ day66.java
@@ -28,15 +28,6 @@ public class day66 {
             }
             System.out.println();
         }
-        System.out.println("====================================================");
-        for (int i = 1; i <= kubus; i++) {
-            for (int j = 1; j <= kubus - i; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("▲");
-            }
-            System.out.println();
-        }
+
     }
 }
