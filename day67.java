28df0329fbe48b8b610173f4f9abefda6969944f
alqn-faqufayoung <alanalvito21@gmail.com>
2024-11-07 18:39:26 +0800

    Method Void

---
+++ day67.java
@@ -0,0 +1,16 @@
+package coding100days;
+
+public class day67 {
+
+    // Method Void
+    /*  metode yang tidak mengembalikan nilai apapun.
+    Method ini melakukan tugas tertentu dan kemudian berhenti tanpa mengembalikan nilai kembali ke pemanggilnya.
+    Kata kunci void digunakan untuk menunjukkan bahwa method tersebut tidak menghasilkan output yang bisa ditangani lebih lanjut oleh program,
+    hanya melakukan aksi atau operasi tertentu. */
+    public static void main(String[] args) {
+        sapaan();
+    }
+    public static void sapaan() {
+        System.out.println("Haii, Saya Alan, Ada yg bisa kami banting..!");
+    }
+}
