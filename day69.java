21da82b445839b840b7e0ed1b5c29c98a79adb19
alqn-faqufayoung <alanalvito21@gmail.com>
2024-11-15 22:15:13 +0800

    evaluasi mentor

---
+++ day69.java
@@ -0,0 +1,16 @@
+package AlanAlvitoFahrurozy;
+
+import java.util.Scanner;
+
+public class soal5 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x;
+        do {
+            System.out.println("masukkan angka: ");
+            x = in.nextInt();
+        } while (x != 9);
+        System.out.println("System berhenti");
+    }
+}
